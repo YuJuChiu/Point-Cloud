60f88373382d5198c43e77906bb849acee8fe833
YuJuChiu <73531190+YuJuChiu@users.noreply.github.com>
2021-07-27 11:09:15 +0200

    Update npy_to_xyznpy.py

--- npy_to_xyznpy.py
+++ npy_to_xyznpy.py
@@ -2,38 +2,23 @@ import os
 import numpy as np
 import glob
 
-# input_path = '/Users/yeelu/desktop/omer_gt_pc_npy/'
+input_path = '/Users/yeelu/desktop/omer_gt_pc_npy/'
 
-# output_path = '/Users/yeelu/desktop/omer_gt_pc_xyznpy/'
+npy_list = glob.glob('/Users/yeelu/desktop/omer_gt_pc_npy/*.npy')
 
-# npy_list = glob.glob('/Users/yeelu/desktop/omer_gt_pc_npy/*.npy')
+for npy in npy_list:
 
-# for npy in npy_list:
+    input = os.path.join(input_path, npy)
 
-#     input = os.path.join(input_path, npy)
+    a = np.load(input, allow_pickle=True)
+    np.asarray(a, dtype = np.float32)
 
-#     a = np.load(input, allow_pickle=True)
-#     np.asarray(a, dtype = np.float32)
+    output = npy[:-4] + '.xyz.npy'
 
-#     output = npy[:-4] + '.xyz.npy'
+    np.save(output, a)
 
-#     np.save(output, a)
 
-input = '/Users/yeelu/desktop/belnsor/429319c0c5bddfccd26c2593d1870bdb.xyz.npy'
 
-# output_path = '/Users/yeelu/desktop/blensor/'
 
-# npy_list = glob.glob('/Users/yeelu/desktop/omer_gt_pc_npy/*.npy')
-
-# for npy in npy_list:
-
-    # input = os.path.join(input_path, npy)
-
-a = np.load(input, allow_pickle=True)
-np.asarray(a, dtype = np.float32)
-
-output = npy[:-8] + '.npy'
-
-np.save(output, a)
 
 
