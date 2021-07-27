07b2c4c842554274a4a0a5fe761f6b0abf96a690
YuJuChiu <73531190+YuJuChiu@users.noreply.github.com>
2021-07-27 10:53:13 +0200

    Update extract_npy_normalize.py

--- extract_npy_normalize.py
+++ extract_npy_normalize.py
@@ -5,7 +5,7 @@ import h5py
 import open3d as o3d
 import glob
 
-# dir to stanford shapenet pc
+# dir to stanford shapenet pc (format: .h5)
 stanford_path = '/Users/yeelu/Desktop/AT3DCV/project/shapenet/train/gt/03001627/'
 
 # dir to shapenet chair mesh
@@ -13,12 +13,6 @@ chair_mesh = '/Users/yeelu/Desktop/omer_mesh/'
 
 # dir to store filtered shapenet mesh
 filter_path = '/Users/yeelu/Desktop/omer_gt_pc_ply/'
-
-# # dir to ouput ply partial pc
-# ply_path = '/Users/yeelu/Desktop/omer_partial_pc_ply/ply/'
-
-# # dir to ouput normalized partial pc
-# normalized_path = '/Users/yeelu/Desktop/omer_partial_pc_ply/normalized/'
 
 stanford_list = os.listdir(stanford_path)
 chair_list = os.listdir(chair_mesh)
@@ -43,11 +37,8 @@ print("DONE with save npy")
 
 partial_file = glob.glob('/Users/yeelu/Desktop/omer_gt_pc_ply/*.npy')
 
-# partial_pc_list = os.listdir(filter_path)
 for par_pc in partial_file:
     partial_pc_file = filter_path + par_pc
-    # print(partial_pc_file)
-    # read as numpy array
     pc_array = np.load(par_pc, allow_pickle=True)
     
     # read as point clound
@@ -63,8 +54,6 @@ print('done with ply conversion')
 npy_pc_list = glob.glob('/Users/yeelu/Desktop/omer_gt_pc_ply/*.ply')
 for pc in npy_pc_list:
 
-    # pc_file = ply_path + pc
-    # read pc
     pcd = o3d.io.read_point_cloud(pc)
     
     # fit to unit cube
