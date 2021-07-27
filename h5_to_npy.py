2856c46588657f9ba9c786907eb14b9ec60df8a5
YuJuChiu <73531190+YuJuChiu@users.noreply.github.com>
2021-07-27 10:57:16 +0200

    Update h5_to_npy.py

--- h5_to_npy.py
+++ h5_to_npy.py
@@ -4,29 +4,21 @@ import open3d as o3d
 import glob
 import os
 
-# h5_files = glob.glob('*.h5')
-h5_files = '1a3127ade9d7eca4fde8830b9596d8b9.h5'
+h5_files = glob.glob('*.h5')
+
 
 BASE_PATH = "/Users/yeelu/Desktop/np_pc"
 
-# for fname in h5_files:
-# 	print(fname)
-# 	f = h5py.File(fname, 'r')
-# 	data = f['data']
-# 	np_data = np.array(data)
+for fname in h5_files:
+	print(fname)
+	f = h5py.File(fname, 'r')
+	data = f['data']
+	np_data = np.array(data)
 
-# 	file_name = "{0}npy".format(fname[:-2])
+	file_name = "{0}npy".format(fname[:-2])
 
-# 	np.save(os.path.join(BASE_PATH, file_name), np_data)
+	np.save(os.path.join(BASE_PATH, file_name), np_data)
 
 
-# print("DONE")
-
-f = h5py.File(h5_files, 'r')
-data = f['data']
-np_data = np.array(data)
-
-file_name = "{0}npy".format(h5_files[:-2])
-
-np.save(os.path.join(BASE_PATH, file_name), np_data)
 print("DONE")
+
